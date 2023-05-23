75807ccea56c9b3190eb23535056a8d9063bd94f
Cai-Huaibin <100181878+Chb123123@users.noreply.github.com>
2023-05-23 11:31:44 +0800

    新增天气图标

---
+++ src/util/weather.js
@@ -0,0 +1,62 @@
+// 多云
+import duoyu from '@/assets/weatherSvg/duoyun.svg'
+// 晴
+import riqing from '@/assets/weatherSvg/riqing.svg'
+// 雷阵雨
+import leizhenyu from '@/assets/weatherSvg/leizhenyu.svg'
+// 冰雹
+import bingbao from '@/assets/weatherSvg/bingbao.svg'
+// 多云转小雨
+import duoyunzhuanxiaoyu from '@/assets/weatherSvg/duoyunzhuanxiaoyu.svg'
+// 多云转中雨
+import duoyunzhuanzhongyu from '@/assets/weatherSvg/duoyunzhuanzhongyu.svg'
+// 大雨
+import dayu from '@/assets/weatherSvg/dabaoyudaotedabaoyu.svg'
+// 小雨
+import xiaoyu from '@/assets/weatherSvg/xiaoyu.svg'
+// 中雨
+import zhongyu from '@/assets/weatherSvg/zhongyu.svg'
+// 阵雨
+import zhenyu from '@/assets/weatherSvg/zhenyu.svg'
+// 阵雪
+import zhenxue from '@/assets/weatherSvg/zhenxue.svg'
+// 小雪
+import xiaoxue from '@/assets/weatherSvg/xiaoxue.svg'
+// 中雪
+import zhongxue from '@/assets/weatherSvg/zhongxue.svg'
+// 大雪
+import daxue from '@/assets/weatherSvg/daxue.svg'
+// 暴雪
+import baoxue from '@/assets/weatherSvg/baoxue.svg'
+// 沙尘暴
+import shachenbao from '@/assets/weatherSvg/shachenbao.svg'
+// 雾霾
+import wumai from '@/assets/weatherSvg/wumai.svg'
+// 阴
+import yin from '@/assets/weatherSvg/yintian.svg'
+// 未匹配
+import zanwushuju from '@/assets/weatherSvg/zanwushuju.svg'
+
+const weatherObj = {
+  '多云': duoyu,
+  '晴': riqing,
+  '雷阵雨': leizhenyu,
+  '冰雹': bingbao,
+  '大雨': dayu,
+  '阴': yin,
+  '小雨': xiaoyu,
+  '中雨': zhongyu,
+  '阵雨': zhenyu,
+  '阵雪': zhenxue,
+  '小雪': xiaoxue,
+  '中雪': zhongxue,
+  '大雪': daxue,
+  '暴雪': baoxue,
+  '沙尘暴': shachenbao,
+  '雾霾': wumai,
+  '多云转中雨': duoyunzhuanzhongyu,
+  '多云转小雨': duoyunzhuanxiaoyu,
+  '无': zanwushuju,
+}
+
+export default weatherObj
